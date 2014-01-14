9ffdb9ab0c302dfbf8026df6400efb1cc589d5fc
Casber <casberw@gmail.com>
2014-01-14 13:07:09 +0800

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ group :test do
   # gem 'win32console', '1.3.2'
   # gem 'wdm', '0.1.0'
   gem 'factory_girl_rails', '4.2.1'
+  gem 'cucumber-rails', '1.4.0', :require => false
+  gem 'database_cleaner', github: 'bmabey/database_cleaner'
 end
 
 gem 'sass-rails', '4.0.1'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -120,6 +121,11 @@ describe User do
   describe "invalid email address with consecutive dots" do
     before { @user.email = "foo@bar..com" }
     it { should_not be_valid }
+  end
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
 
